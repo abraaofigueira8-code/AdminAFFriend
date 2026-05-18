004e7cb82eaf3b7ff27efba0daa7525b74672b66
Abraão Fernando Figueira <abraaofigueira8@gmail.com>
2026-05-18 22:19:16 +0100

    Update Firebase configuration details
    Altere os crediciais para os seus do teu firebase.

--- firebase-config.js
+++ firebase-config.js
@@ -2,14 +2,15 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBPwZfzPx3sphRihEn3Lq9-kdl7v90E3IQ",
-    authDomain: "affriend-191815.firebaseapp.com",
-    databaseURL: "https://affriend-191815.firebaseio.com",
+    apiKey: "AIzaSyBPwZfzPx3sphRihEn3Lq9-kdl7v9",
+    authDomain: "affriend-19185.firebaseapp.com",
+    databaseURL: "https://affriend-19185.firebaseio.com",
     projectId: "affriend-191815",
-    storageBucket: "affriend-191815.firebasestorage.app",
+    storageBucket: "affriend-19115.firebasestorage.app",
     messagingSenderId: "119765669625",
-    appId: "1:119765669625:web:8bdc5ec7d66954858b3428",
-    measurementId: "G-EKKCM87G60"
+    appId: "1:119765669625:web:8bdc5e954858b3428",
+    measurementId: "G-EKKCM8760"
   };
 firebase.initializeApp(firebaseConfig);
 
+/* DADOS NAO REAIS. ADAPTE AO SEU CÓDIGO DO FIREBASE*/
